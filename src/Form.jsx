a6eea61626d8aa1e15ef96e4c3118de8c54304d1
naatii <jcorber855@g.educaand.es>
2023-07-14 13:05:22 +0200

    Agregando el formulario temporal

--- src/Form.jsx
+++ src/Form.jsx
@@ -16,65 +16,70 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './Form.css'
 
 function From() {
+  const form = document.getElementById("formulario")
+  const enviar  = addEventListener("submit", ()=>{
+
+  })
   return (
-    <Form id='formulario'>
-      <Row className="mb-3">
+    // <Form id='formulario' action="mailto:cateringdepepes@gmail.com">
+    //   <Row className="mb-3">
 
-        <Form.Group as={Col} controlId="Name">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Nombre" />
-          <Form.Text>EL nombre no puede contener números</Form.Text>
-        </Form.Group>
+    //     <Form.Group as={Col} controlId="Name">
+    //       <Form.Label>Nombre</Form.Label>
+    //       <Form.Control type="text" placeholder="Nombre" />
+    //       <Form.Text>EL nombre no puede contener números</Form.Text>
+    //     </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="example@gmail.com" />
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridAddress1">
-            <Form.Label>Dirección</Form.Label>
-            <Form.Control placeholder="C/ España" />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridAddress1">
-            <Form.Label>Número de puerta</Form.Label>
-            <Form.Control placeholder="12" />
-        </Form.Group>
-        </Row>
+    //     <Form.Group as={Col} controlId="formGridEmail">
+    //       <Form.Label>Email</Form.Label>
+    //       <Form.Control type="email" placeholder="example@gmail.com" />
+    //     </Form.Group>
+    //   </Row>
+    //   <Row className="mb-3">
+    //     <Form.Group as={Col} controlId="formGridAddress1">
+    //         <Form.Label>Dirección</Form.Label>
+    //         <Form.Control placeholder="C/ España" />
+    //     </Form.Group>
+    //     <Form.Group as={Col} controlId="formGridAddress1">
+    //         <Form.Label>Número de puerta</Form.Label>
+    //         <Form.Control placeholder="12" />
+    //     </Form.Group>
+    //     </Row>
         
-      <Form.Group className="mb-3" controlId="formGridNumber">
-        <Form.Label>Número</Form.Label>
-        <Form.Control type='tel' placeholder="634 126 753" />
-      </Form.Group>
+    //   <Form.Group className="mb-3" controlId="formGridNumber">
+    //     <Form.Label>Número</Form.Label>
+    //     <Form.Control type='tel' placeholder="634 126 753" />
+    //   </Form.Group>
 
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
-        </Form.Group>
+    //   <Row className="mb-3">
+    //     <Form.Group as={Col} controlId="formGridCity">
+    //       <Form.Label>Provincia</Form.Label>
+    //       <Form.Control />
+    //     </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
-          </Form.Select>
-        </Form.Group>
+    //     <Form.Group as={Col} controlId="formGridState">
+    //       <Form.Label>Ciuadad</Form.Label>
+    //       <Form.Select defaultValue="Choose...">
+    //         <option>Choose...</option>
+    //         <option>...</option>
+    //       </Form.Select>
+    //     </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
+    //     <Form.Group as={Col} controlId="formGridZip">
+    //       <Form.Label>Código postal</Form.Label>
+    //       <Form.Control />
+    //     </Form.Group>
+    //   </Row>
 
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+    //   <Form.Group className="mb-3" id="formGridCheckbox">
+    //     <Form.Check type="checkbox" label="Recuérdame" />
+    //   </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    //   <Button variant="primary" type="submit">
+    //     Submit
+    //   </Button>
+    // </Form>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdg_vybYPo6K9-QkphahYozylzfkpt8IoE0rzFo8wI6DlgUag/viewform?pli=1&pli=1&pli=1&edit2=2_ABaOnuc46IjL3MmkuLE5nxqXF87RTyB41mvU4VxPXJq_BeAlhNGAqncbDduCTd7XUw" frameborder="1"></iframe>
   );
 }
 
