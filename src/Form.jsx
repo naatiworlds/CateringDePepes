8648c63212a5c37d31bb48c08ff8db14711057b2
naatii <jcorber855@g.educaand.es>
2023-07-14 14:18:25 +0200

    Pruebas con formularios

--- src/Form.jsx
+++ src/Form.jsx
@@ -16,21 +16,70 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './Form.css'
 
 function From() {
- 
+  const form = document.getElementById("formulario")
+  const enviar  = addEventListener("submit", ()=>{
+
+  })
   return (
-    <>
-      <form name="contacto" method="POST" data-netlify="true">
-      <label htmlFor="nombre">Nombre</label>
-      <input type="text" name="nombre" id="nombre" placeholder="Su nombre"/>
-      <label htmlFor="correo">Correo</label>
-      <input type="email" name="correo" id="correo" placeholder="example@gmail.com"/>
-      <label htmlFor="numero">número</label>
-      <input type="tel" name="numero" id="numero" placeholder="687 78 89 12"/>
-      <textarea name="opinion" id="opinion" cols="30" rows="10" placeholder="Añada un comentario aquí"></textarea>
-      <input type="hidden" name="form-name" value="contact" />
-      <button type="submit">Enviar</button>
-    </form>
-    </>
+    <Form id='formulario' name="contacto" data-netlify="true" netlify>
+      <Row className="mb-3">
+
+        <Form.Group as={Col} controlId="Name">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control type="text" placeholder="Nombre" />
+          <Form.Text>EL nombre no puede contener números</Form.Text>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="example@gmail.com" />
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridAddress1">
+            <Form.Label>Dirección</Form.Label>
+            <Form.Control placeholder="C/ España" />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridAddress1">
+            <Form.Label>Número de puerta</Form.Label>
+            <Form.Control placeholder="12" />
+        </Form.Group>
+        </Row>
+        
+      <Form.Group className="mb-3" controlId="formGridNumber">
+        <Form.Label>Número</Form.Label>
+        <Form.Control type='tel' placeholder="634 126 753" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>Provincia</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>Ciuadad</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Código postal</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Recuérdame" />
+        <Form.Check type="hidden" name="form-name" value="contact" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     // <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdg_vybYPo6K9-QkphahYozylzfkpt8IoE0rzFo8wI6DlgUag/viewform?pli=1" frameborder="1"></iframe>
   );
 }
