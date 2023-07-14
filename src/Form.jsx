f7dd5fff67b6350f8f36494814365dd507b4d27b
naatii <jcorber855@g.educaand.es>
2023-07-14 19:54:34 +0200

    Pruebas con formularios

--- src/Form.jsx
+++ src/Form.jsx
@@ -16,32 +16,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './Form.css'
 
 function From() {
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
+  
   return (
 
-    <form 
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}> 
+    <form name="contact">
       <input type="hidden" name="form-name" value="contact" />
 
       <label htmlFor="nombre">Nombre</label>
