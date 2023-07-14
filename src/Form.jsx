2d2b485380c8c1aa4c726c841db1752f61751938
naatii <jcorber855@g.educaand.es>
2023-07-14 13:18:06 +0200

    Pruebas con formularios

--- src/Form.jsx
+++ src/Form.jsx
@@ -21,7 +21,7 @@ function From() {
 
   })
   return (
-    <Form id='formulario' data-netlify="true">
+    <Form id='formulario' name="contacto" method="POST" data-netlify="true">
       <Row className="mb-3">
 
         <Form.Group as={Col} controlId="Name">
